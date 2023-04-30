a5a13d0a2dd37048ff0d80d14ce06c55569ce48c
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-04-30 01:55:42 -0700

    modified deployment pipeline

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,5 @@ cargo fmt --all
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
 mv tailwindcss-linux-x64 tailwindcss
-cargo install --locked trunk
 trunk build
 git subtree push --prefix dist origin gh-pages
