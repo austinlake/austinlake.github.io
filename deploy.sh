bc0296e930059d8a64d7e50355d413065530fef1
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-04-30 00:28:01 -0700

    fixed github action config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 cargo test
-rustup component add clippy
 cargo clippy -- -Dwarnings
 cargo fmt --all
-cargo install --locked trunk
 trunk build
 git subtree push --prefix dist origin gh-pages
