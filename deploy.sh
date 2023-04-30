261e5673ca46d172a632378bf4371065c6b7dbff
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-04-30 01:38:08 -0700

    included tailwindcss download in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ cargo test
 cargo clippy -- -Dwarnings
 rustup component add rustfmt --toolchain nightly-x86_64-unknown-linux-gnu
 cargo fmt --all
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+chmod +x tailwindcss-linux-x64
+mv tailwindcss-linux-x64 tailwindcss
 cargo install --locked trunk
 trunk build
 git subtree push --prefix dist origin gh-pages
