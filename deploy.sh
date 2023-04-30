91d98d7ca485c483e3105b6076bd421df10393fb
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-04-30 00:13:40 -0700

    fixed dependency issue in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 cargo test
+rustup component add clippy
 cargo clippy -- -Dwarnings
 cargo fmt --all
+cargo install --locked trunk
 trunk build
 git subtree push --prefix dist origin gh-pages
